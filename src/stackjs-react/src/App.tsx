e289a093314036667183556244e7580cf9c86de9
confessore <confessore@balasolu.com>
2022-09-28 20:18:28 -0400

    react components

--- src/stackjs-react/src/App.tsx
+++ src/stackjs-react/src/App.tsx
@@ -1,31 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-          <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
-            <blockquote>
-              <p className="text-lg font-medium">
-                "Yo, this Tailwind CSS on React stuff is pretty dope.
-                It turns up quick and looks fly as hell. My people love it."
-              </p>
-            </blockquote>
-            <figcaption className="font-medium">
-              <div className="text-sky-500 dark:text-sky-400">
-                Steven Confessore
-              </div>
-              <div className="text-slate-700 dark:text-slate-500">
-                Staff Engineer, United States
-              </div>
-            </figcaption>
-          </div>
-        </figure>
       </header>
+      <Footer />
     </div>
   );
 }
